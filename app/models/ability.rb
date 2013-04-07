8234b5df692a069e4aef63b2a747472cb792942b
iamlemur <joelleimer@gmail.com>
2013-04-07 14:00:24 +0400

    added basic logout capability
    - hard coded logout link and added login and sign up links
    - needs to be updated with proper routes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,7 @@ class Ability
     end
     can :read, Post
     can :create, Post
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
